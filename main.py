eec2be1b4a5a6a69398f0ff7a04e483894945365
Daniel Kriz <147802055+Daniel908009@users.noreply.github.com>
2024-12-12 18:58:28 +0100

    So this is my idea, first I will check if some move wins the game imediately, if not then I will do something like the minimax method.
    I dont think it will end up exactly like minimax though, because I want to do my own thing.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from kivy.app import App
 from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.popup import Popup
+import random
 
 # this is the popup class for settings
 class SettingsPopup(Popup):
@@ -10,6 +11,8 @@ class SettingsPopup(Popup):
         self.caller = caller
         self.ids.gridSize.text = str(caller.grid_size)
         self.ids.winCount.text = str(caller.win_count)
+        self.ids.aiPlayer.text = caller.ai_player
+        self.ids.aiDifficulty.text = caller.ai_difficulty
     def apply_settings(self):
         if self.ids.gridSize.text != '' and int(self.ids.gridSize.text) > 0 and self.ids.winCount.text != '' and int(self.ids.winCount.text) > 0 and int(self.ids.winCount.text) <= int(self.ids.gridSize.text):
             self.caller.grid_size = int(self.ids.gridSize.text)
@@ -30,7 +33,7 @@ class GridButton(Button):
         if self.text == '':
             self.text = self.caller.current_player
             self.caller.current_player = 'X' if self.caller.current_player == 'O' else 'O'
-            self.caller.check_winner(self, False, self.caller.grid)
+            self.caller.check_winner(self, self.caller.grid)
             if self.caller.ai_player == 'AI' and self.text == 'X':
                 self.ai_move()
     def ai_move(self):
@@ -42,35 +45,70 @@ class GridButton(Button):
         elif self.caller.ai_difficulty == 'Hard':
             depth = 6
         # getting the best move
-        best_move = self.ai_choice(1)
-        print(best_move)
+        best_move = self.ai_choice(depth)
+        #print(best_move)
         # making the move
-        #self.caller.grid[best_move[0]][best_move[1]].text = 'O'
-        #self.caller.check_winner(self.caller.grid[best_move[0]][best_move[1]], True, copy_grid)
-        #self.caller.current_player = 'X'
+        self.caller.grid[best_move[0]][best_move[1]].text = 'O'
+        self.caller.check_winner(self.caller.grid[best_move[0]][best_move[1]], self.caller.grid)
+        self.caller.current_player = 'X'
     def ai_choice(self, depth):
         index_depth = 0
         moves = []
-        i = 0
-        while i < len(self.caller.grid):
-            j = 0
-            while j < len(self.caller.grid[i]):
-                if self.caller.grid[i][j].text == '':
-                    moves.append([i, j])
-                j += 1
-            i += 1
-        #print(moves)
-        while index_depth < depth:
-            print('depth:', index_depth)
-            for move in moves:
-                copy_grid = self.caller.grid.copy()
-                copy_grid[move[0]][move[1]].text = 'O'
-                #print(self.caller.check_winner(copy_grid[move[0]][move[1]], True, copy_grid))
-                if self.caller.check_winner(copy_grid[move[0]][move[1]], True, copy_grid):
-                    print('winning move at move:', move)
-                    return move
-
-            index_depth += 1
+        moves = [(0,0),(0,1), (0,4), (0,2)]
+        moves_to_remove = []
+        for move in moves:
+            print(move)
+            if self.caller.grid[move[0]][move[1]].text == 'O':
+                moves_to_remove.append(move)
+        for move in moves_to_remove:
+            moves.remove(move)
+        # getting all the possible moves
+        #for i in range(self.caller.grid_size):
+        #    for j in range(self.caller.grid_size):
+        #        if self.caller.grid[i][j].text == '':
+        #            moves.append([i, j])
+        # if there is a winning move, return it
+        print(moves)
+        for move in moves:
+            self.caller.grid[move[0]][move[1]].text = 'O'
+            if self.check_if_winner(move):
+                print('winning move')
+                return move
+            self.caller.grid[move[0]][move[1]].text = ''
+        return moves[0]
+        
+    def check_if_winner(self, move):
+        copy_grid = []
+        #print(move)
+        for i in range(self.caller.grid_size):
+            copy_grid.append([])
+            for j in range(self.caller.grid_size):
+                copy_grid[i].append(self.caller.grid[i][j].text)
+        #print(move)
+        #print(copy_grid)
+        copy_grid[move[0]][move[1]] = 'O'
+        # checking both horizontal options
+        checked = 0
+        buttons_of_player = 0
+        while buttons_of_player < self.caller.win_count+1 and checked < self.caller.win_count:
+            try:
+                if copy_grid[move[0]][move[1] + checked] == 'O':
+                    buttons_of_player += 1
+            except:
+                break
+            checked += 1
+        checked = 0
+        while buttons_of_player < self.caller.win_count+1 and checked < self.caller.win_count:
+            try:
+                if copy_grid[move[0]][move[1] - checked] == 'O':
+                    buttons_of_player += 1
+            except:
+                break
+            checked += 1
+        #print(buttons_of_player)
+        if buttons_of_player >= self.caller.win_count + 1:
+            return True
+        return False
 
 # this is the main grid class of the app
 class MainGrid(GridLayout):
@@ -96,7 +134,7 @@ class MainGrid(GridLayout):
         # its a bit weird to set the cols, and then return it as well, but it is necessary and it works
         game_grid.cols = self.grid_size
         return self.grid_size
-    def check_winner(self, button_clicked, called_by_ai, grid):
+    def check_winner(self, button_clicked,grid):
         #print(called_by_ai, button_clicked.text)
         buttons_of_player = 0
         winner_buttons = []
@@ -120,7 +158,7 @@ class MainGrid(GridLayout):
                 break
             checked += 1
         if buttons_of_player >= self.win_count + 1: # the plus one is there because the button that was clicked last is counted twice
-            self.winner(button_clicked.text, winner_buttons, called_by_ai)
+            self.winner(button_clicked.text, winner_buttons)
         # checking both vertical options
         buttons_of_player = 0
         checked = 0
@@ -143,7 +181,7 @@ class MainGrid(GridLayout):
                 break
             checked += 1
         if buttons_of_player >= self.win_count + 1:
-            self.winner(button_clicked.text, winner_buttons, called_by_ai)
+            self.winner(button_clicked.text, winner_buttons)
         # checking the first pair of diagonal options
         buttons_of_player = 0
         checked = 0
@@ -166,7 +204,7 @@ class MainGrid(GridLayout):
                 break
             checked += 1
         if buttons_of_player >= self.win_count + 1:
-            self.winner(button_clicked.text, winner_buttons, called_by_ai)
+            self.winner(button_clicked.text, winner_buttons)
         # checking the second pair of diagonal options
         buttons_of_player = 0
         checked = 0
@@ -188,17 +226,13 @@ class MainGrid(GridLayout):
                 break
             checked += 1
         if buttons_of_player >= self.win_count + 1:
-            self.winner(button_clicked.text, winner_buttons, called_by_ai)
-    def winner(self, winner, winner_buttons, called_by_ai):
-        if not called_by_ai: 
-            self.ids.currentPlayer.text = 'Winner is: ' + winner
-            for button in self.ids.gameGrid.children:
-                button.disabled = True
-            for button in winner_buttons:
-                button.background_color = (0, 1, 0, 1)
-        else:
-            print("somewhere is an ai winner move")
-            return True
+            self.winner(button_clicked.text, winner_buttons)
+    def winner(self, winner, winner_buttons): 
+        self.ids.currentPlayer.text = 'Winner is: ' + winner
+        for button in self.ids.gameGrid.children:
+            button.disabled = True
+        for button in winner_buttons:
+            button.background_color = (0, 1, 0, 1)
 
 # app class
 class TicTacToeApp(App):
